4772649f597882a9f92b21a7503033c10f03dc4d
Rohit Raut <61349517+Rohit-Raut@users.noreply.github.com>
2025-08-18 20:27:56 -0400

    Create colors.lua

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,19 @@
+return{
+    {
+        "folke/tokyonight.nvim",
+        config = function()
+            require("tokyonight").setup({transparent=true})
+            vim.cmd.colorscheme "tokyonight"
+        end
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies={
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts ={
+            theme = 'tokyonight',
+        }
+    },
+
+}
